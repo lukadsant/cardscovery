9ab3da4b90c081ca075342d25b17f4b0456b9fb4
Luka <68041537+lukadsant@users.noreply.github.com>
2023-04-10 08:53:40 -0300

    Update settings.py

--- geeks/settings.py
+++ geeks/settings.py
@@ -114,6 +114,12 @@ USE_I18N = True
 USE_TZ = True
 
 
+
+CSRF_TRUSTED_ORIGINS = [
+    'cardscovery.up.railway.app'
+]
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
